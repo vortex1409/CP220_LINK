3ecd1934252d5d917cb6c30cf02ec5a2ba76df02
Michael Dorfman <mvd1409@gmail.com>
2017-03-23 11:18:27 -0400

    Added some code

--- CP220_LINK/Program.cs
+++ CP220_LINK/Program.cs
@@ -10,6 +10,69 @@ namespace CP220_LINK
     {
         static void Main(string[] args)
         {
+            //Link<int> myLink = new Link<int>(16);
+            //Debug.WriteLine(myLink.Data);
+
+            //myLink.insertAfter(new Link<int>(21));
+
+            //Debug.WriteLine(myLink.nextValue.Data);
+
         }
+
+        public class Link<T>
+        {
+            private Link<T> _nextLink;
+            private Link<T> _previousLink;
+            private T value;
+
+            public Link(T parm)
+            {
+                value = parm;
+            }
+
+            public Link<T> nextLink
+            {
+                get
+                {
+                    return _nextLink;
+                }
+
+                set
+                {
+                    _nextLink = value;
+                }
+            }
+            public Link<T> previousLink
+            {
+                get
+                {
+                    return _previousLink;
+                }
+
+                set
+                {
+                    _previousLink = value;
+                }
+            }
+            public T data
+            {
+                get
+                {
+                    return value;
+                }
+            }
+
+            public void remove()
+            {
+                nextLink.previousLink = previousLink;
+                previousLink.nextLink = nextLink;
+            }
+
+            public void insertAfter(Link<T> newObj)
+            {
+
+            }
+        }
+
     }
 }
